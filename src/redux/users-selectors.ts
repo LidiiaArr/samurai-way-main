48186b87dd2a0baba0b35f0fde6870ad74e5b39e
Lidiia Arbekova <lida1450@yandex.ru>
2023-09-27 11:13:01 +0300

    83 reselect

--- src/redux/users-selectors.ts
+++ src/redux/users-selectors.ts
@@ -1,8 +1,18 @@
 import {AppStateType} from "./redux-store";
+import {createSelector} from "reselect";
 
 export const getUsersPage = (state: AppStateType) => {
     return state.usersPage;
 }
+
+export const getUsersSelector = (state: AppStateType) => {
+    return state.usersPage.users
+}
+
+export const getUsersSuperSelectors = createSelector(getUsersSelector,(users)=> {
+    return users.filter(u => true)
+})
+//кешируем селектор
 
 export const getPageSize = (state: AppStateType) => {
     return state.usersPage.pageSize;
